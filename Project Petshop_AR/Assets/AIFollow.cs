2e96fa65b51d6b295fe21a495901c008f260ce0e
SolomonGrunty <wburdick18@gmail.com>
2020-02-08 18:17:50 -0600

    Updated AR to allow pet to follow user

---
+++ Project Petshop_AR/Assets/AIFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFollow : MonoBehaviour
+{
+    private Transform owner;
+    private Rigidbody rb;
+    public float speed = 5f;
+    public float maxDistance = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        owner = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(owner.position, transform.position);
+        if (distance >= maxDistance)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, owner.position, step);
+        }
+    }
+}
